031d9018847ca274b4ed4edd286e06ece4bf72bf
Aryan <aryanbaghel040@gmail.com>
2025-05-05 17:20:54 +0530

    few changes 2.2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,11 +16,8 @@ const Navbar = () => {
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <Link
-              type="button"
               href={`/dashboard/${account?.address}`}
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
             >
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
