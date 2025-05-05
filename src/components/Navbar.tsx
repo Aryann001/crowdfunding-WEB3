919d13c5fb6df9844ce30ce5020772d87a377399
Aryan <aryanbaghel040@gmail.com>
2025-05-05 16:49:03 +0530

    few changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        <button type="button"  className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                        <button type="button" onClick={()=>router.push(`/dashboard/${account?.address}`)} className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                         <span className="absolute -inset-0.5"></span>
                         <span className="sr-only">Open main menu</span>
                         <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
@@ -26,8 +26,8 @@ const Navbar = () => {
                         </svg>
                         </button>
                     </div>
-                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                        <div className="flex flex-shrink-0 items-center">
+                    <div className="flex flex-1 items-center -translate-x-10 justify-center sm:translate-x-0 sm:items-stretch sm:justify-start">
+                        <Link href={'/'} className="flex flex-shrink-0 items-center">
                             <Image 
                                 src={crowdfundingIcon} 
                                 alt="Crowdfunding" 
@@ -37,7 +37,7 @@ const Navbar = () => {
                                     filter: "drop-shadow(0px 0px 24px #a726a9a8)",
                                 }}
                             />
-                        </div>
+                        </Link>
                         <div className="hidden sm:ml-6 sm:block">
                             <div className="flex space-x-4">
                                 <Link
