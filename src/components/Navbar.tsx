7e5bb7830032b765631620241f1615bd7cb12257
Aryan <aryanbaghel040@gmail.com>
2025-05-05 17:02:36 +0530

    few changes 2.1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,9 +15,9 @@ const Navbar = () => {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button
+            <Link
               type="button"
-              onClick={() => router.push(`/dashboard/${account?.address}`)}
+              href={`/dashboard/${account?.address}`}
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
@@ -52,7 +52,7 @@ const Navbar = () => {
                   d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
-            </button>
+            </Link>
           </div>
           <div className="flex flex-1 items-center -translate-x-10 justify-center sm:translate-x-0 sm:items-stretch sm:justify-start">
             <Link href={"/"} className="flex flex-shrink-0 items-center">
